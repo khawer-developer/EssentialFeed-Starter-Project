bdb8a26d3f5875747b256d05bdb284dda0acbb66
Khawer Khaliq <khawerkhaliq@gmail.com>
2025-09-20 23:40:20 +0400

    'RemoteFeedLoader' delivers connectivity error on client error

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -8,10 +8,17 @@
 import Foundation
 
 public protocol HTTPClient {
-    func get(from url: URL)
+    func get(
+        from url: URL,
+        completion: (Error) -> Void
+    )
 }
 
 public final class RemoteFeedLoader {
+    public enum Error: Swift.Error {
+        case connectivity
+    }
+    
     private let url: URL
     private let client: HTTPClient
     
@@ -23,8 +30,10 @@ public final class RemoteFeedLoader {
         self.client = client
     }
     
-    public func load() {
-        client.get(from: url)
+    public func load(completion: (Error) -> Void = { _ in }) {
+        client.get(from: url) { error in
+            completion(.connectivity)
+        }
     }
 }
 
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -40,6 +40,24 @@ class RemoteFeedLoaderTests: XCTestCase {
         )
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        client.error = NSError(
+            domain: "Test",
+            code: 0
+        )
+        var capturedError: RemoteFeedLoader.Error?
+        
+        sut.load { error in
+            capturedError = error
+        }
+        
+        XCTAssertEqual(
+            capturedError,
+            .connectivity
+        )
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -53,8 +71,15 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var error: Error?
         
-        func get(from url: URL) {
+        func get(
+            from url: URL,
+            completion: (Error) -> Void
+        ) {
+            if let error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
