b476518d4077a37a7117235e3c3cbb50b512e50f
Khawer Khaliq <khawerkhaliq@gmail.com>
2025-10-11 23:07:06 +0400

    Resumes data task on creation

--- EssentialFeed/EssentialFeedTests/FeedAPITests/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPITests/URLSessionHTTPClientTests.swift
@@ -15,7 +15,7 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL) {
-        session.dataTask(with: url) { _, _, _ in }
+        session.dataTask(with: url) { _, _, _ in }.resume()
     }
 }
 
@@ -33,19 +33,52 @@ class URLSessionHTTPClientTests: XCTestCase {
         )
     }
     
+    func test_getFromURL_resumesDataTaskWithURL() {
+        let url = URL(string: "https://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(
+            url: url,
+            task: task
+        )
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(
+            task.resumeCallCount,
+            1
+        )
+    }
+    
     // MARK: - Helpers
     
     private class URLSessionSpy: URLSession, @unchecked Sendable {
         var receivedURLs = [URL]()
+        private var stubs = [URL: URLSessionDataTask]()
+        
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
+        }
         
         override func dataTask(
             with url: URL,
             completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
         ) -> URLSessionDataTask {
             receivedURLs.append(url)
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
         }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable {
+        override func resume() { }
+    }
+    
+    private class URLSessionDataTaskSpy: URLSessionDataTask, @unchecked Sendable {
+        var resumeCallCount = 0
         
-        private class FakeURLSessionDataTask: URLSessionDataTask, @unchecked Sendable { }
+        override func resume() {
+            resumeCallCount += 1
+        }
     }
 }
