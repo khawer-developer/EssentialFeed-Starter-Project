04487aec5d67cc1dd7af5507f87991afb1cf3d16
Khawer Khaliq <khawerkhaliq@gmail.com>
2025-10-26 17:53:01 +0400

    Extract test setup to a helper method

--- EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeed/EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -10,22 +10,9 @@ import EssentialFeed
 
 final class EssentialFeedAPIEndToEndTests: XCTestCase {
     func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
-        let loader = RemoteFeedLoader(
-            url: testServerURL,
-            client: client
-        )
-        let exp = expectation(description: "Wait for load completion")
-        var receivedResult: LoadFeedResult?
+        let feedResult = getFeedResult()
         
-        loader.load { result in
-            receivedResult = result
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 5.0)
-        
-        switch receivedResult {
+        switch feedResult {
         case let .success(items)?:
             XCTAssertEqual(
                 items.count,
@@ -72,6 +59,23 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    
+    private func getFeedResult() -> LoadFeedResult? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(
+            url: testServerURL,
+            client: client
+        )
+        let exp = expectation(description: "Wait for load completion")
+        var receivedResult: LoadFeedResult?
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        return receivedResult
+    }
     
     private func expectedItem(at index: Int) -> FeedItem {
         FeedItem(
